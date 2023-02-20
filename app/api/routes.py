e7c7ac2ff1c43fb85a1b4fe3f6c03892b67025ae
Jacob Bailey <jayembailey@gmail.com>
2023-02-20 18:03:44 -0500

    2nd commit - up to api routes

---
+++ app/api/routes.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, request, jsonify, render_template
+from helpers import token_required
+from models import db, User, Car, car_schema, cars_schema
+
+api = Blueprint('api', __name__, url_prefix='/api')
+
+@api.route('/getdata')
+def getdata():
+    return {'oh': 'yeah'}
+
+@api.route('/my_cars', methods = ['POST'])
+@token_required
+def create_car(current_user_token):
+    year = request.json['year']
+    make = request.json['make']
+    model = request.json['model']
+    color = request.json['color']
+    user_token = current_user_token.token
+
+    print(f'CREATE TESTER: {current_user_token.token}')
+
+    car = Car(year, make, model, color, user_token = user_token)
+
+    db.session.add(car)
+    db.session.commit()
+
+    response = car_schema.dump(car)
+    return jsonify(response)
+
+@api.route('/my_cars', methods = ['GET'])
+@token_required
+def get_contact(current_user_token):
+    a_user = current_user_token.token
+    cars = Car.query.filter_by(user_token = a_user)
+    response = cars_schema.dump(cars)
+    return jsonify(response)
+
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config():
     FLASK_APP = os.getenv('FLASK_APP')
     FLASK_ENV = os.getenv('FLASK_ENV')
-    SECRET_KEY = os.environ.get('SUPER_SECRET_KEYYY')
+    SECRET_KEY = 'SUPER_SECRET_KEYYY'
+    WTF_CSRF_SECRET_KEY = 'SECRET_SECRET_SECRET'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_NOTIFICATIONS = False
